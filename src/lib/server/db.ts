e301f9ff9edc163c2628db3f5220a896cdf0a0c6
ZachF <zfrankel2016@gmail.com>
2025-02-06 17:09:22 +0000

    Created db.ts for future use

---
+++ src/lib/server/db.ts
@@ -0,0 +1,13 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('db.db');
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  )`);
+});
+
+export { db };
